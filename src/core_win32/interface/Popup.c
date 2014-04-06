ea07ed3bb392388bd3f39e69304c94ab723155af
jobtalle <jobtalle@hotmail.com>
2014-04-06 22:12:25 +0200

    Fixed popup.c naming conflict

--- src/core_win32/interface/Popup.c
+++ src/core_win32/interface/Popup.c
@@ -1,11 +1,11 @@
 #include "Popup.h"
 
-void showPopup(char* message, char* caption, ccPopupType type)
+void ccShowPopup(char* message, char* caption, ccPopupType type)
 {
 	MessageBox(NULL, message, caption, type);
 }
 
-bool showDialogue(char* message, char* caption, ccDialogueType type)
+bool ccShowDialogue(char* message, char* caption, ccDialogueType type)
 {
 	return MessageBox(NULL, message, caption, type) == 0 ? false : true;
 }
