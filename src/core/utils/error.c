84734f55b75a4bfceb926159ad53f3fccc2b26fd
Thomas Versteeg <thomasversteeg@gmx.com>
2014-04-23 21:05:05 +0200

    Added error to string function and fixed debug warnings

--- src/core/utils/error.c
+++ src/core/utils/error.c
@@ -1,5 +1,27 @@
 #include "error.h"
 
+void ccErrorString(char *dest, ccError error)
+{
+	const char *str;
+
+	switch(error){
+		case ccErrorNo:
+			str = "No errors";
+			break;
+		case ccErrorNoDisplay:
+			str = "Could not open display";
+			break;
+		case ccErrorGLVersionUnsupported:
+			str = "The current OpenGL version is not supported";
+			break;
+		default:
+			str = "Error message not defined";
+			break;
+	}
+
+	strcpy(dest, str);
+}
+
 void ccAbort(const char* message)
 {
 	ccShowPopup(message, "fatal error", ccPopupError);
--- src/core/utils/error.h
+++ src/core/utils/error.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string.h>
 #include "../interface/popup.h"
 
 typedef enum {
