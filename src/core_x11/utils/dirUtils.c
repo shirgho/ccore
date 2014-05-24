40d4623729d62ee473807fb98a72b3300d9276d6
Thomas Versteeg <thomasversteeg@gmx.com>
2014-05-24 16:53:26 +0200

    X11 added default locations

---
+++ src/core_x11/utils/dirUtils.c
@@ -0,0 +1,16 @@
+#include "dirUtils.h"
+
+char *ccGetUserDir()
+{
+	return CC_USER_LOCATION;
+}
+
+char *ccGetDataDir()
+{
+	return CC_DATA_LOCATION;
+}
+
+char *ccGetTempDir()
+{
+	return CC_TEMP_LOCATION;
+}
---
+++ src/core_x11/utils/dirUtils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../core/utils/dirUtils.h"
+
+#ifndef CC_USER_LOCATION
+#define CC_USER_LOCATION "~/.config/"
+#endif
+
+#ifndef CC_DATA_LOCATION
+#define CC_DATA_LOCATION ""
+#endif
+
+#ifndef CC_TEMP_LOCATION
+#define CC_TEMP_LOCATION "/tmp/"
+#endif
