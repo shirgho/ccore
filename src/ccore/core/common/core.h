7149f23cf9034223297aef5de6f9b28988ec3783
jobtalle <jobtalle@hotmail.com>
2014-06-08 14:34:49 +0200

    Removed redundant include

--- src/ccore/core/common/core.h
+++ src/ccore/core/common/core.h
@@ -2,12 +2,7 @@
 
 #ifdef __linux__
 #include "../linux/core.h"
-#ifdef GTK
-#include "../gtk/core.h"
-#else
-#define X11
 #include "../x11/core.h"
-#endif
 
 #elif defined _WIN32
 #include "../win32/core.h"
