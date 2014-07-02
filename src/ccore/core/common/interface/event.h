535e9120d9f65db7923114b6d5b6b9ed37a5cbf4
jobtalle <jobtalle@hotmail.com>
2014-07-02 13:14:00 +0200

    Simplified key event
    keycodes will now be constants mapping to scan codes

--- src/ccore/core/common/interface/event.h
+++ src/ccore/core/common/interface/event.h
@@ -38,107 +38,6 @@ typedef enum {
 	CC_MOUSE_BUTTON_SPECIAL_2 = 5
 } ccMouseButtonType;
 
-// A type of keyboard action, used inside the ccEvent union
-// Mapped on ASCII where possible
-typedef enum {
-	CC_KEY_UNDEFINED = 0,
-
-	CC_KEY_BACKSPACE = 8,
- 	CC_KEY_TAB = '\t',
-	CC_KEY_RETURN = 13,
-	CC_KEY_ESCAPE = 27,
-	CC_KEY_SPACE = ' ',
-
-	CC_KEY_0 = '0',
-	CC_KEY_1 = '1',
-	CC_KEY_2 = '2',
-	CC_KEY_3 = '3',
-	CC_KEY_4 = '4',
-	CC_KEY_5 = '5',
-	CC_KEY_6 = '6',
-	CC_KEY_7 = '7',
-	CC_KEY_8 = '8',
-	CC_KEY_9 = '9',
-	CC_KEY_A = 'A',
-	CC_KEY_B = 'B',
-	CC_KEY_C = 'C',
-	CC_KEY_D = 'D',
-	CC_KEY_E = 'E',
-	CC_KEY_F = 'F',
-	CC_KEY_G = 'G',
-	CC_KEY_H = 'H',
-	CC_KEY_I = 'I',
-	CC_KEY_J = 'J',
-	CC_KEY_K = 'K',
-	CC_KEY_L = 'L',
-	CC_KEY_M = 'M',
-	CC_KEY_N = 'N',
-	CC_KEY_O = 'O',
-	CC_KEY_P = 'P',
-	CC_KEY_Q = 'Q',
-	CC_KEY_R = 'R',
-	CC_KEY_S = 'S',
-	CC_KEY_T = 'T',
-	CC_KEY_U = 'U',
-	CC_KEY_V = 'V',
-	CC_KEY_W = 'W',
-	CC_KEY_X = 'X',
-	CC_KEY_Y = 'Y',
-	CC_KEY_Z = 'Z',
-
-	CC_KEY_CAPSLOCK,
-	CC_KEY_INSERT,
-	CC_KEY_DELETE,
-	CC_KEY_HOME,
-	CC_KEY_END,
-	CC_KEY_PAGEUP,
-	CC_KEY_PAGEDOWN,
-
-	CC_KEY_PRINTSCREEN,
-	CC_KEY_SCROLLLOCK,
-	CC_KEY_NUMLOCK,
-	CC_KEY_PAUSEBREAK,
-
-	CC_KEY_F1,
-	CC_KEY_F2,
-	CC_KEY_F3,
-	CC_KEY_F4,
-	CC_KEY_F5,
-	CC_KEY_F6,
-	CC_KEY_F7,
-	CC_KEY_F8,
-	CC_KEY_F9,
-	CC_KEY_F10,
-	CC_KEY_F11,
-	CC_KEY_F12,
-
-	CC_KEY_NUM0,
-	CC_KEY_NUM1,
-	CC_KEY_NUM2,
-	CC_KEY_NUM3,
-	CC_KEY_NUM4,
-	CC_KEY_NUM5,
-	CC_KEY_NUM6,
-	CC_KEY_NUM7,
-	CC_KEY_NUM8,
-	CC_KEY_NUM9,
-
-	CC_KEY_LCONTROL,
-	CC_KEY_RCONTROL,
-	CC_KEY_LSHIFT,
-	CC_KEY_RSHIFT,
-
-	CC_KEY_LEFT,
-	CC_KEY_RIGHT,
-	CC_KEY_UP,
-	CC_KEY_DOWN
-} ccKeyCode;
-
-typedef struct {
-	ccKeyCode keyCode; //CCORE supported key code described in the enum above
-	unsigned short scanCode; //hardware dependent unique scancode, for custom key mapping
-} ccKeyData;
-
 // The event structure used by ccWindow, set by the call ccPollEvent
 typedef struct {
 	// The type of data inside the union depends on the type of event
@@ -147,7 +46,7 @@ typedef struct {
 	union {
 		ccMouseButtonType mouseButton;
 		ccPoint mousePosition;
-		ccKeyData key;
+		char keyCode;
 		short scrollDelta;
 	};
 } ccEvent;
