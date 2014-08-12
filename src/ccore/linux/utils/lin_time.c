59bbb3539ad5cffd6d045b9f3389f91cf7945c1f
Job Talle <jobtalle@hotmail.com>
2014-08-12 17:36:58 +0200

    LINUX - fixed timing cast

--- src/ccore/linux/utils/lin_time.c
+++ src/ccore/linux/utils/lin_time.c
@@ -5,9 +5,9 @@ void ccTimeDelay(int ms)
 	usleep(ms * _TO_MICROSECONDS);
 }
 
-unsigned long ccTimeNanoseconds()
+uint64_t ccTimeNanoseconds()
 {
 	struct timespec time;
 	clock_gettime(CLOCK_REALTIME, &time);
-	return time.tv_nsec + time.tv_sec * _TO_SECONDS;
+	return (uint64_t)(time.tv_nsec + time.tv_sec * _TO_SECONDS);
 }
