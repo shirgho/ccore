dd85295d2ed158801b6a74773c4a1235cbd9a3cc
Job Talle <jobtalle@hotmail.com>
2014-08-13 15:02:31 +0200

    LINUX - added threading related files

---
+++ src/ccore/linux/utils/lin_thread.c
@@ -0,0 +1,33 @@
+#include "lin_thread.h"
+
+ccError ccThreadCreate(ccThread *thread, void *function)
+{
+	ccMalloc(*thread, sizeof(ccThread_lin));
+	((ccThread_lin*)*thread)->function = function;
+	return CC_ERROR_NONE;
+}
+
+ccError ccThreadStart(ccThread thread, void *data)
+{
+	if(pthread_create(&_THREAD->id, NULL, _THREAD->function, data) != 0) {
+		free(thread);
+		return CC_ERROR_THREAD;
+	}
+	return CC_ERROR_NONE;
+}
+
+ccError ccThreadJoin(ccThread thread)
+{
+	return pthread_join(_THREAD->id, NULL) == 0 ? CC_ERROR_NONE : CC_ERROR_THREAD;
+}
+
+bool ccThreadFinished(ccThread thread)
+{
+	if(pthread_kill(_THREAD->id, 0) == 0)
+	{
+		return false;
+	}else{
+		free(thread);
+		return true;
+	}
+}
---
+++ src/ccore/linux/utils/lin_thread.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <pthread.h>
+
+#include <ccore/thread.h>
+
+typedef struct {
+	pthread_t id;
+	void *function;
+} ccThread_lin;
+
+#define _THREAD ((ccThread_lin*)thread)
