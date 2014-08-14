52f43f6a5e152baecf47b3692b3fd74fd8c8a49a
jobtalle <jobtalle@hotmail.com>
2014-08-14 15:53:23 +0200

    LINUX - added gamepad placeholder files to prevent terrible crashes

---
+++ src/ccore/linux/interface/lin_gamepad.c
@@ -0,0 +1,16 @@
+#include "lin_gamepad.h"
+
+ccError ccGamepadConnect()
+{
+	return CC_ERROR_NONE;
+}
+
+int ccGamepadCount()
+{
+	return 0;
+}
+
+void ccGamepadDisconnect()
+{
+
+}
---
+++ src/ccore/linux/interface/lin_gamepad.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include <ccore/gamepad.h>
