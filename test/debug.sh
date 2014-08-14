c25c0f1bbb4e35fca3d3d45ee5ba5c7fafb7c639
Thomas Versteeg <thomasversteeg@gmx.com>
2014-08-14 15:11:37 +0200

    Test removed redundant files

--- test/debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-if scons -Q
-then 
-	xterm -e "sleep 2 && gdb -tui -x=~/.config/gdbinit ./featuretest && scons -Q -c" &
-	valgrind --vgdb=yes --vgdb-error=0 ./featuretest
-fi
