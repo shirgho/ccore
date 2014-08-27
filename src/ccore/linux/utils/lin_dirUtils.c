000e958c4e28013d4e803a4fba05edf923f80373
Thomas Versteeg <thomasversteeg@gmx.com>
2014-08-27 11:29:02 +0200

    Linux fixed ccGetDirDataDir memory leak

--- src/ccore/linux/utils/lin_dirUtils.c
+++ src/ccore/linux/utils/lin_dirUtils.c
@@ -9,19 +9,15 @@ char *ccGetDirDataDir(void)
 {
 #ifndef CC_DATA_LOCATION
 	char *buf;
+	int len;
 
 	buf = malloc(PATH_MAX);
-	readlink("/proc/self/exe", buf, PATH_MAX);
-
-	buf = dirname(buf);
-	strcat(buf, "/");
-
-	/* TODO discuss if the last / needs te be removed
-	if(buf[strlen(buf) - 1] == '/'){
-		buf[strlen(buf) - 1] = '\0';
+	len = readlink("/proc/self/exe", buf, PATH_MAX);
+	if(len > 0){
+		buf[len] = '\0';
+		buf = dirname(buf);
+		strcat(buf, "/");
 	}
-	*/
-
 	return buf;
 #else
 	return CC_DATA_LOCATION;
