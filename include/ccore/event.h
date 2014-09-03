e8c6ef2e413a239e967657101bfa1ddc0cb64354
jobtalle <jobtalle@hotmail.com>
2014-09-03 15:32:43 +0200

    Removed mysterious tab

--- include/ccore/event.h
+++ include/ccore/event.h
@@ -33,55 +33,55 @@ extern "C"
 {
 #endif
 
-	// A type of event returned used in ccWindow after the function ccPollEvent is called
-	typedef enum {
-		// Event not handled in ccore, return false on pollEvent
-		CC_EVENT_SKIP,
-		// A key is pressed, store keycode
-		CC_EVENT_KEY_DOWN,
-		// A key is released, store keycode
-		CC_EVENT_KEY_UP,
-		// Mouse has moved, store coordinates
-		CC_EVENT_MOUSE_MOVE,
-		// Mouse button down, store buttoncode
-		CC_EVENT_MOUSE_DOWN,
-		// Mouse button up, store buttoncode
-		CC_EVENT_MOUSE_UP,
-		// Mouse wheel moved up
-		CC_EVENT_MOUSE_SCROLL,
-		// The keyboard is focussed on the window
-		CC_EVENT_FOCUS_GAINED,
-		// The keyboard lost focus on the window
-		CC_EVENT_FOCUS_LOST,
-		// Window close button was pressed
-		CC_EVENT_WINDOW_QUIT,
-		// Window has resized, new dimensions are stored in the window struct before the event fires
-		CC_EVENT_WINDOW_RESIZE,
-		// Gamepad event, see gamepad.h for specifics
-		CC_EVENT_GAMEPAD
-	} ccEventType;
+// A type of event returned used in ccWindow after the function ccPollEvent is called
+typedef enum {
+	// Event not handled in ccore, return false on pollEvent
+	CC_EVENT_SKIP,
+	// A key is pressed, store keycode
+	CC_EVENT_KEY_DOWN,
+	// A key is released, store keycode
+	CC_EVENT_KEY_UP,
+	// Mouse has moved, store coordinates
+	CC_EVENT_MOUSE_MOVE,
+	// Mouse button down, store buttoncode
+	CC_EVENT_MOUSE_DOWN,
+	// Mouse button up, store buttoncode
+	CC_EVENT_MOUSE_UP,
+	// Mouse wheel moved up
+	CC_EVENT_MOUSE_SCROLL,
+	// The keyboard is focussed on the window
+	CC_EVENT_FOCUS_GAINED,
+	// The keyboard lost focus on the window
+	CC_EVENT_FOCUS_LOST,
+	// Window close button was pressed
+	CC_EVENT_WINDOW_QUIT,
+	// Window has resized, new dimensions are stored in the window struct before the event fires
+	CC_EVENT_WINDOW_RESIZE,
+	// Gamepad event, see gamepad.h for specifics
+	CC_EVENT_GAMEPAD
+} ccEventType;
 
-	// The type of mouse button pressed, used inside the ccEvent union
-	typedef enum {
-		CC_MOUSE_BUTTON_NONE,
-		CC_MOUSE_BUTTON_LEFT,
-		CC_MOUSE_BUTTON_MIDDLE,
-		CC_MOUSE_BUTTON_RIGHT
-	} ccMouseButtonType;
+// The type of mouse button pressed, used inside the ccEvent union
+typedef enum {
+	CC_MOUSE_BUTTON_NONE,
+	CC_MOUSE_BUTTON_LEFT,
+	CC_MOUSE_BUTTON_MIDDLE,
+	CC_MOUSE_BUTTON_RIGHT
+} ccMouseButtonType;
 
-	// The event structure used by ccWindow, set by the call ccPollEvent
-	typedef struct {
-		ccEventType type;
+// The event structure used by ccWindow, set by the call ccPollEvent
+typedef struct {
+	ccEventType type;
 
-		// The union contains different data types for different events
-		union {
-			ccMouseButtonType mouseButton;
-			ccPoint mouseDelta;
-			unsigned int keyCode;
-			float scrollDelta;
-			ccGamepadEvent gamepadEvent;
-		};
-	} ccEvent;
+	// The union contains different data types for different events
+	union {
+		ccMouseButtonType mouseButton;
+		ccPoint mouseDelta;
+		unsigned int keyCode;
+		float scrollDelta;
+		ccGamepadEvent gamepadEvent;
+	};
+} ccEvent;
 
 #ifdef X11
 #include <X11/keysym.h>
